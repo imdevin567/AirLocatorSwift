d045acc042bc7cde736dbffd7b15c5c702f39354
Devin Young <devin.young@topgolf.com>
2014-06-07 13:02:40 -0700

    Initial Commit

---
+++ AirLocatorSwiftTests/AirLocatorSwiftTests.swift
@@ -0,0 +1,35 @@
+//
+//  AirLocatorSwiftTests.swift
+//  AirLocatorSwiftTests
+//
+//  Created by Devin Young on 6/7/14.
+//  Copyright (c) 2014 Devin Young. All rights reserved.
+//
+
+import XCTest
+
+class AirLocatorSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
